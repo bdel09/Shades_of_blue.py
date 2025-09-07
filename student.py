f7aacf99962e4de415196efc7b4fca617c7774c6
bdel09 <bmcdelbarrio@gmail.com>
2025-09-07 15:07:45 -0400

    Update student.py

--- student.py
+++ student.py
@@ -7,7 +7,7 @@ tess = turtle.Turtle()
 tess.shape()
 tess.backwards(100)
 
-for i in range(0,255,10):
+for i in range(0,260,10):
     tess.forward(10)
     tess.pensize(i)
-    tess.color(0,0,i)
+    tess.color(0,i,0)
